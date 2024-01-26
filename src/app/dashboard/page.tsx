4d93c6fea6b46ede7123009884394509d02d9b0c
Tim <kimtimmy93@gmail.com>
2024-01-26 15:14:21 -0800

    fix auth-callback page issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 
-const Page = () => {
+const Page = async () => {
   const { getUser } = getKindeServerSession()
-  const user = getUser()
-
+  const user = await getUser()
   if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
   
   return (
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,17 +1,32 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
-    const {getUser} = getKindeServerSession()
-    const user = getUser()
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-    if (!user.id || !user.email) throw new TRPCError({ code: 'UNAUTHORIZED' })
+    if (!user?.id || !user?.email) throw new TRPCError({ code: 'UNAUTHORIZED' });
 
-    return {success: true}
-  })
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
+  }),
 });
-
 
 export type AppRouter = typeof appRouter;
