fd22fb7eda7d4055d4cc0f95597c17054913fd43
Tim <kimtimmy93@gmail.com>
2024-01-26 15:20:38 -0800

    add dbuser check logic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,21 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
+import { db } from '@/db';
 
 const Page = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-  if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
-  
-  return (
-    <div>{user.email}</div>
-  )
-}
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user || !user.id) redirect('/auth-callback?origin=dashboard');
+
+  const dbUser = await db.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect('/auth-callback?origin=dashboard');
+
+  return <div>{user.email}</div>;
+};
 
 export default Page;
