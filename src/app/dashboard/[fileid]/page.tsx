4da2d297e72fb5ba2fb69d446ada442e55a2fbcb
Tim <kimtimmy93@gmail.com>
2024-01-30 12:30:46 -0800

    add left/right sides of pdf viewer

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,6 +1,8 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { notFound, redirect } from 'next/navigation';
-import { db } from '@/db'
+import { db } from '@/db';
+import PdfRenderer from '@/components/PdfRenderer';
+import ChatWrapper from '@/components/ChatWrapper';
 
 interface PageProps {
   params: {
@@ -24,7 +26,23 @@ const Page = async ({ params }: PageProps) => {
 
   if (!file) notFound();
 
-  return <div>{fileid}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* keft side */}
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+            <PdfRenderer />
+          </div>
+        </div>
+        
+        {/* right side */}
+        <div className='shrink-0-flex flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:-border-t-0'>
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,9 @@
+const PdfRenderer = () => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14"></div>
+    </div>
+  );
+};
+
+export default PdfRenderer;
