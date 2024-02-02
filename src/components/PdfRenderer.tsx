0933ab5d4168e1cb3049447e4afe0f27e6b92688
Tim <kimtimmy93@gmail.com>
2024-02-01 23:35:21 -0800

    add pdf view

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,9 +1,27 @@
-const PdfRenderer = () => {
+'use client';
+
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
+interface PdfRendererProps {
+  url: string
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className='flex items-center gapy-1.5'>
-          top bar
+          
+        </div>
+      </div>
+      <div className='flex-1 w-full max-h-screen'>
+        <div>
+          <Document file={url} className='max-h-full'>
+            <Page pageNumber={1} />
+          </Document>
         </div>
       </div>
     </div>
