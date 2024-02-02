a5617590170030a069a7e1f8565946d8023eb67a
Tim <kimtimmy93@gmail.com>
2024-02-01 23:40:47 -0800

    add resize detector to PDf

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,26 +1,45 @@
 'use client';
 
+import { Loader2 } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import { useToast } from './ui/use-toast';
+import { useResizeDetector } from 'react-resize-detector';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
-  url: string
+  url: string;
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-        <div className='flex items-center gapy-1.5'>
-          
-        </div>
+        <div className="flex items-center gapy-1.5"></div>
       </div>
-      <div className='flex-1 w-full max-h-screen'>
-        <div>
-          <Document file={url} className='max-h-full'>
-            <Page pageNumber={1} />
+      <div className="flex-1 w-full max-h-screen">
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="h-6 w-6 animate-spin my-24" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: 'Error loading PDF',
+                description: 'Please try again later',
+                variant: 'destructive',
+              });
+            }}
+            file={url}
+            className="max-h-full"
+          >
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
