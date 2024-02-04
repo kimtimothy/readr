62eee5ac25f689277eebc66d46309be6d36ac7d7
Tim <kimtimmy93@gmail.com>
2024-02-03 22:06:15 -0800

    add chat input status messages

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,9 @@
+interface ChatInputProps {
+  isDisabled?: boolean;
+}
+
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  return <div className="flex-1 justify-between flex flex-col mb-28"></div>;
+};
+
+export default ChatInput;
---
+++ src/components/chat/Messages.tsx
@@ -0,0 +1,5 @@
+const Messages = () => {
+  return <div className="flex-1 justify-between flex flex-col mb-28"></div>;
+};
+
+export default Messages;
